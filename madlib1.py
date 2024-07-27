b8231dab326b0b61e88c16144b20c01ebf9383cd
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2024-07-27 00:45:05 +0000

    first madlib drafted

---
+++ madlib1.py
@@ -0,0 +1,25 @@
+place_in_office = input("Name a place in the Office: ")
+number1 = input("Pick a number between 1 and 10: ")
+worst_job_title = input("The worst Job title ever: ")
+Fake_tech_company = input("The fakest fake name company of all time: ")
+verb = input("pick a verb infinitive: ")
+devops = input("Describe devops badly: ")
+devops2 = input("Describe devops accurately: ")
+reality = input("app you use the the most throughout the day: ")
+verbing = input("action verb ending in -ing: ")
+bad_app = input("Fake Name for an app that hates easy integerations with other apps: ")
+boss = input("best last name ever: ")
+material1 = input("Worst building material ever: ")
+material2 = input("Something you would never use as glue ever: ")
+puppet = input("Describe puppetry badly: ")
+
+
+
+
+print(f"Once upon a cronjob, Litlle Sammy was getting ready for a day of work at {Fake_tech_company}. Little Sammy, or Samtholemew the Stout for short, was a Staff {worst_job_title} at {Fake_tech_company}. Which meant their job was to {devops}. But some people said it was more like {devops2}. But really it was spending too much time on {reality}. \n")
+
+print(f"Little Sammy, or 'Samsonite, I was way off!' for short, was {verbing} to the office that day for some reason. They were having a rough time with it. Well when they got to {Fake_tech_company} and put their computer on their desk in the {place_in_office}. Time to get all {number1} of these employees input into the new HR app, {bad_app}. Suddenly, Monsieur{boss} ran into the {place_in_office}. 'SAMSONITE I WAS WAY OFF!' Señor {boss} shouted disdainfully. 'I told you to get all {number1} of these employees uploaded. What is taking so long? \n")
+
+print(f"'Well, {boss}-kun, I tried telling you that this was way too many people for this app! IT's help together with {material1} and {material2}. \n")
+
+print(f"'If I wanted your opinion I'd {puppet}. Now Listen up!' {boss} began detailing his plan, and it turned out the plan was to replace Little Sammy, or Sittle Lammy for short, with an LLM model. TO BE CONTINUED \n")
