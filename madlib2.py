01fcef46c18f899e44d0527ae5e7768a37ab0ab6
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2024-07-29 17:31:08 +0000

    madlib 2 and 3

---
+++ madlib2.py
@@ -0,0 +1,45 @@
+import time
+
+ai_name = input("Bad name for an AI company: ")
+ai_model_name = input("Seriously, a really bad name for an AI programmer bot thing: ")
+spice = input("The lost sixth spice girl's nickname: ")
+insult = input("The kind of insult a four year old who is not allowed to swear but is also not very mean would use: ")
+verbing = input("A verb ending in -ing: ")
+past_tense = input("Past tense verb: ")
+animal = input("Animal: ")
+relationship = input("Complicated relationship. Cousin's mother's etc. etc.: ")
+object = input("Name a object that is difficult to hold with one hand: ")
+shirt = input("Company name that rhymes with Browdspike: ")
+verbed = input("past tense verb: ")
+
+
+print(f"We return now to our tale of Little Sammy, or The artist formerly know as AmperSam for short. Little Sammy was shocked to learn that they were about to be replaced by AI. 'Well who makes this AI that wil replace me?' \n 'Well, that was build by {ai_name} and it's called {ai_model_name}. It was built by {spice}. \n")
+
+print(f"'AW shucks. Well I think {ai_model_name} is a {insult}. And I'm not gonna mince words about it,' Little Sammy said right in their boss' face. \n 'Ok.' said the boss. \n  ")
+
+print(f"... \n")
+print(f"... \n")
+
+time.sleep(2)
+
+print(f"... 'So are you gonna turn on the AI or...' \n 'We're waiting on you to leave, Little Sammy!\n" )
+
+print(f" Later, Little Sammy, or Lil Sam - first of his name of house Sam for short, was {verbing} on the curb outside of work. 'What am I gonna do now?' Little Sammy {past_tense}. Just then, a cloud of smoke appeared and from it emerged a {animal}. 'BE NOT AFRAID. I AM YOU'RE FAIRY {relationship}. I've come to make you more productive! \n ")
+print(f" 'I CAN'T BE PRODUCTIVE ANYMORE, FAIRY {relationship}! I got fired! \n")
+print(f" OH REALLY? WELL I'M NOT THE ONE WHO CAN HELP. YOU NEED BOB. \n")
+print(f"Bob?\n")
+print(f"Yeah. Bob.\n")
+print(f"..\n.")
+
+time.sleep(2)
+
+print("...\n") 
+time.sleep(2)
+print(f" 'So are you gonna get Bob or...\n'")
+print(f"'Oh snap sure. Sorry' the Fairy {relationship} waved their magic {object} and suddenly a bored looking fella in a {shirt} shirt appeared. \n")
+print(f" 'I don't trust his shirt' Little Sammy whispered to the Fairy {relationship}.\n")
+print(f"'Hey. I'm Bob. Wish granted.'\n")
+print(f"But I didn't wish for anything!' Little Sammy {verbed}.\n")
+print(f"'Gosh, he's good' whispered the Fairy {relationship}. \n")
+
+print(f" To be FINISHED, next time!")
